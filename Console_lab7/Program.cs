091619eb76184936e465066b0b9de881a92977ce
Ira <mazuririnochkacat@gmail.com>
2023-04-13 19:48:32 +0300

    Добавьте файлы проекта.

---
+++ Console_lab7/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Console_lab7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Task1
+            Console.Write("Enter size your array: ");
+            int s = int.Parse(Console.ReadLine());
+            Array Myarray = new Array(s);
+            Myarray.Show();
+
+
+            Console.Write("information message: ");
+            string inform = Console.ReadLine();
+            Myarray.Show(inform);
+        }
+    }
+}
---
+++ Console_lab7/Task1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_lab7
+{
+    interface IOutput
+    {
+        void Show();
+        void Show(string info);
+    }
+
+    class Array : IOutput
+    {
+        public int Size { get; set; }
+        public Array(int size)
+        {
+            this.Size = size;
+        }
+
+        public void Show()
+        {
+            int[] arr = new int[Size];
+            Random rnd = new Random();
+            Console.Write("Array: ");
+            for (int i = 0; i < Size; i++)
+            {
+                arr[i] = rnd.Next(1, 25);
+                Console.Write(arr[i] + "  ");
+            }
+        }
+        public void Show(string inform)
+        {
+            Show();
+            Console.WriteLine($"Information message: {inform}");
+        }
+    }
+}
